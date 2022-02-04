3f470f93a7dd32d86670f64d7bcf11b446ed569f
Abelost <98475355+Abelost@users.noreply.github.com>
2022-02-04 10:11:39 +0100

    Add files via upload
    Both receiving battery % and sending speed of 90

--- remote_control_rfnano.ino
+++ remote_control_rfnano.ino
@@ -24,26 +24,28 @@ int maxSpeed = 179;
 
 RF24 radio(CE_PIN, CSN_PIN);
 
+const uint64_t writingAddress = 0x9090909001;
+const uint64_t readingAddress = 0x9090909002;
 //const byte addresses[][6] = {"Mauri", "Lovaa"};
-const uint64_t rfAddr = 0x9090909001;
+//const uint64_t rfAddr = 0x9090909001;
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
   printf_begin();
-  //radio.openWritingPipe(addresses[1]); // 00002
-  //radio.openReadingPipe(1, addresses[0]); // 00001
-  radio.openWritingPipe(rfAddr);
+  radio.openWritingPipe(writingAddress);
+  radio.openReadingPipe(1, readingAddress);
+  //radio.openWritingPipe(rfAddr);
   radio.setPALevel(RF24_PA_MIN);
 
-  // radio.setDataRate( DATARATE ) ;
+  //radio.setDataRate( DATARATE ) ;
   radio.setChannel(0x54);
   radio.enableDynamicPayloads() ;
   radio.enableAckPayload();               // not used here
   radio.setRetries(0, 15);                // Smallest time between retries, max no. of retries
   radio.setAutoAck( true ) ;
 
-  radio.printDetails();
+  radio.printDetails();                   //Print for debugging
   radio.powerUp();
   pinMode (yPin, INPUT);
 }
@@ -51,7 +53,7 @@ void setup() {
 void loop() {
   delay(5);
 
-  //radio.stopListening();
+  radio.stopListening();
 //  int yPos = analogRead(yPin);
 //  ySpeed = map(yPos, 0, 1023, 0, 180); // kommer bli 0, 1023, 1000, 2000 vid ESC
 //
@@ -87,13 +89,13 @@ void loop() {
 
   delay(5);
   
-//  radio.startListening();
-//  if ( radio.available()) {
-//    int text = "";
-//    radio.read(&text, sizeof(text));
-//    Serial.print("Battery is: ");
-//    Serial.print(text);
-//  }
+  radio.startListening();
+  if ( radio.available()) {
+    int text = "";
+    radio.read(&text, sizeof(text));
+    Serial.print("Battery is: ");
+    Serial.print(text);
+  }
 
 
 
